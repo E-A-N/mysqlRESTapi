d30a86a4e027f6d7fc19b0b2ce9b8181a01798c4
Cyborg_Ean <ean.director@gmail.com>
2017-11-28 10:29:17 -0800

    currently working on routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const task = require("../models/task");
+
+
+const renderJson (err, rows) => {
+    if (err) {
+        res.json(err);
+    }
+    else {
+        res.json(rows);
+    }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:id?', (req, res, next) => {
+    const userID = req.params.id;
+    if (userID) task.getTaskById(userID,renderJson);
 });
 
 module.exports = router;
